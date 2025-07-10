79ed11dddd3912e34355b1c3c8acff3646ff7a19
shagohead <html.ru@gmail.com>
2025-07-10 12:12:19 +0300

    Add context token source

--- talentv2/security.go
+++ talentv2/security.go
@@ -3,13 +3,16 @@ package talentv2
 import (
 	"context"
 
+	"github.com/go-faster/errors"
 	"github.com/ogen-go/ogen/ogenerrors"
 )
 
+// Source of credentials for client_credentials OAuth grant.
 type ClientCredentialsSource interface {
 	Credentials(context.Context) (clientID, clientSecret string, err error)
 }
 
+// Source of Talent OAuth access_token.
 type TalentTokenSource interface {
 	Token(context.Context) (string, error)
 }
@@ -24,6 +27,7 @@ func (c *clientCredentials) Credentials(context.Context) (clientID string, clien
 	return c.id, c.secret, nil
 }
 
+// ClientCredentialsSecurity returns [SecuritySource] applicable only for client_credentials grant.
 func ClientCredentialsSecurity(clientID, clientSecret string) SecuritySource {
 	return Security(&clientCredentials{
 		id:     clientID,
@@ -31,11 +35,42 @@ func ClientCredentialsSecurity(clientID, clientSecret string) SecuritySource {
 	}, nil)
 }
 
+// Security returns [SecuritySource] which can be used as access_token source and/or client_credentials source.
+// Both sources can be nil. If source is nil, appropriate security requirement will be ignored.
+// If both of them nil, this SecuritySource can be used only for unauthenticated calls.
 func Security(ccsrc ClientCredentialsSource, tauth TalentTokenSource) SecuritySource {
 	return &securitySource{
 		ccsrc: ccsrc,
 		tauth: tauth,
 	}
+}
+
+type ctxKey int
+
+const (
+	contextToken ctxKey = iota
+)
+
+// ContextTokenSource implements [TalentTokenSource] which uses token from context.
+// Set token before making request with [ContextWithToken] function.
+func ContextTokenSource() TalentTokenSource {
+	return &contextTokenSource{}
+}
+
+type contextTokenSource struct{}
+
+var ErrContextMissingToken = errors.New("context missing token")
+
+// Token implements TalentTokenSource.
+func (c *contextTokenSource) Token(ctx context.Context) (string, error) {
+	if v := ctx.Value(contextToken); v != nil {
+		return v.(string), nil
+	}
+	return "", ErrContextMissingToken
+}
+
+func ContextWithToken(ctx context.Context, token string) context.Context {
+	return context.WithValue(ctx, contextToken, token)
 }
 
 type securitySource struct {
